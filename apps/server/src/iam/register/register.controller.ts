cd046d3e65b72dc2f61d62d25d81ff6eb9c91583
Ahmed Mahmoud Azzam <ahmed195138@feng.bu.edu.eg>
2024-08-13 17:15:29 +0300

    init

---
+++ apps/server/src/iam/register/register.controller.ts
@@ -0,0 +1,42 @@
+import {
+  Controller,
+  Post,
+  Body,
+  HttpStatus,
+  BadRequestException,
+  HttpCode,
+} from '@nestjs/common';
+import { RegisterService } from './register.service';
+import { RegisterUserDto } from './dto/register-user.dto';
+import { ApiBadRequestResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { AuthType } from '../login/enums/auth-type.enum';
+import { AuthGuard } from '../login/decorators/auth-guard.decorator';
+
+@ApiTags('auth')
+@AuthGuard(AuthType.None)
+@Controller('auth/register')
+export class RegisterController {
+  constructor(private readonly registerService: RegisterService) {}
+
+  @Post()
+  @HttpCode(201)
+  @ApiOkResponse({
+    status: 201,
+    description:
+      'Register a new user and send a confirmation email to the user',
+  })
+  @ApiBadRequestResponse({ status: 400, description: 'Bad request' })
+  public async register(
+    @Body() registerUserDto: RegisterUserDto,
+  ): Promise<any> {
+    try {
+      await this.registerService.register(registerUserDto);
+      return {
+        message: 'User registration successfully!',
+        status: HttpStatus.CREATED,
+      };
+    } catch (err) {
+      throw new BadRequestException(err, 'Error: User not registration!');
+    }
+  }
+}
---
+++ apps/server/src/users/users.controller.ts
@@ -0,0 +1,130 @@
+import {
+  Controller,
+  Put,
+  Get,
+  Body,
+  Param,
+  HttpStatus,
+  NotFoundException,
+  Delete,
+  BadRequestException,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { UserProfileDto } from './dto/user-profile.dto';
+import { UserUpdateDto } from './dto/user-update.dto';
+import { AccountsUsers } from './interfaces/accounts-users.interface';
+import {
+  ApiBadRequestResponse,
+  ApiBearerAuth,
+  ApiNoContentResponse,
+  ApiNotFoundResponse,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { AuthGuard } from '../iam/login/decorators/auth-guard.decorator';
+import { AuthType } from '../iam/login/enums/auth-type.enum';
+
+@ApiTags('users')
+@ApiBearerAuth()
+@AuthGuard(AuthType.Bearer)
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  @ApiResponse({
+    status: 200,
+    description: 'Get all users',
+  })
+  public async findAllUser(): Promise<AccountsUsers[]> {
+    return this.usersService.findAll();
+  }
+
+  @Get('/:userId')
+  @ApiResponse({
+    status: 200,
+    description: 'Get a user by id',
+  })
+  @ApiNotFoundResponse({ status: 400, description: 'User not found' })
+  public async findOneUser(
+    @Param('userId') userId: string,
+  ): Promise<AccountsUsers> {
+    return this.usersService.findById(userId);
+  }
+
+  @Get('/:userId/profile')
+  @ApiResponse({
+    status: 200,
+    description: 'Get a user profile by id',
+  })
+  @ApiNotFoundResponse({ status: 400, description: 'User not found' })
+  public async getUser(@Param('userId') userId: string): Promise<any> {
+    const user = await this.findOneUser(userId);
+
+    if (!user) {
+      throw new NotFoundException('User does not exist!');
+    }
+
+    return {
+      user: user,
+      status: HttpStatus.OK,
+    };
+  }
+
+  @Put('/:userId/profile')
+  @ApiResponse({
+    status: 200,
+    description: 'Update a user profile by id',
+  })
+  @ApiBadRequestResponse({
+    status: 400,
+    description: 'User profile not updated',
+  })
+  public async updateUserProfile(
+    @Param('userId') userId: string,
+    @Body() userProfileDto: UserProfileDto,
+  ): Promise<any> {
+    try {
+      await this.usersService.updateUserProfile(userId, userProfileDto);
+
+      return {
+        message: 'User Updated successfully!',
+        status: HttpStatus.OK,
+      };
+    } catch (err) {
+      throw new BadRequestException(err, 'Error: User not updated!');
+    }
+  }
+
+  @Put('/:userId')
+  @ApiResponse({
+    status: 200,
+    description: 'Update a user by id',
+  })
+  @ApiBadRequestResponse({ status: 400, description: 'User not updated' })
+  public async updateUser(
+    @Param('userId') userId: string,
+    @Body() userUpdateDto: UserUpdateDto,
+  ): Promise<any> {
+    try {
+      await this.usersService.updateUser(userId, userUpdateDto);
+
+      return {
+        message: 'User Updated successfully!',
+        status: HttpStatus.OK,
+      };
+    } catch (err) {
+      throw new BadRequestException(err, 'Error: User not updated!');
+    }
+  }
+
+  @Delete('/:userId')
+  @ApiResponse({
+    status: 200,
+    description: 'Delete a user by id',
+  })
+  @ApiNoContentResponse({ status: 404, description: 'User not deleted' })
+  public async deleteUser(@Param('userId') userId: string): Promise<void> {
+    await this.usersService.deleteUser(userId);
+  }
+}
